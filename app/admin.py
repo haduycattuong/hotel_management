357f7e8b53f2f58b0aa6bf9d4480a6a3c1b886ce
haduycattuong <haduy.cattuong611@gmail.com>
2023-12-30 19:41:44 +0700

    ADD: create view for admin

--- app/admin.py
+++ app/admin.py
@@ -18,7 +18,7 @@ class AuthenticatedUser(BaseView):
 
 
 class StatsView(AuthenticatedUser):
-    @expose("/")
+    @expose("/api/reports")
     def index(self):
         return self.render('admin/stats.html')
 
@@ -29,16 +29,106 @@ class LogoutView(AuthenticatedUser):
         logout_user()
         return redirect('/admin')
 
+        
+class AdminView(AuthenticatedAdmin):
+  can_delete = False
+  can_edit = False
+  can_create = True
+
+class BookingView(AuthenticatedAdmin):
+    column_list = ['id', 'guest_id', 'status_id', 'num_guest', 'has_foreigner', 'booking_price',
+                   'check_in', 'check_out', 'created_at']    
+    column_searchable_list = ['guest_id', 'status_id', 'num_guest', 'has_foreigner', 'booking_price',
+                   'check_in', 'check_out', 'created_at']
+    column_editable_list = ['status_id', 'num_guest', 'has_foreigner', 'booking_price', 'created_at']
+
+    can_export = True
+    can_delete = True
+    can_edit = True
+
+class StatusView(AuthenticatedAdmin):
+    column_list = ['id', 'status']    
+    column_searchable_list = ['status']
+    column_editable_list = ['status']
+
+    can_export = True
+    can_delete = True
+    can_edit = True
+    can_create = True
+class RoomView(AuthenticatedAdmin):
+    column_list = ['id', 'name', 'description', 'foreigner_rate']    
+    column_searchable_list = ['name']
+    column_editable_list = ['name', 'description', 'foreigner_rate']
+
+    can_delete = True
+    can_edit = True
+    can_create = True
+
+class RoomTypeView(AuthenticatedAdmin):
+    column_list = ['id', 'type', 'price', 'max_capacity']    
+    column_searchable_list = ['type', 'price']
+    column_editable_list = ['type', 'price', 'max_capacity']
+
+    can_delete = True
+    can_edit = True
+    can_create = True
+
+class PaymentView(AuthenticatedAdmin):
+    column_list = ['id', 'price', 'description']    
+    column_searchable_list = ['price']
+    column_editable_list = ['description', 'price']
+
+    can_delete = True
+    can_edit = True
+    can_create = True
+    can_export = True
+
+class PaymentMethodView(AuthenticatedAdmin):
+    column_list = ['id', 'method']    
+    column_searchable_list = ['method']
+    column_editable_list = ['id', 'method']
+
+    can_delete = True
+    can_edit = True
+    can_create = True
+
+class AdditionalPriceView(AuthenticatedAdmin):
+    column_list = ['id', 'price_rate', 'price_value']    
+    column_searchable_list = ['price_rate', 'price_value']
+    column_editable_list = ['price_rate', 'price_value']
+
+    can_delete = True
+    can_edit = True
+    can_create = True
+
+class GuestView(AuthenticatedAdmin):
+    column_list = ['id', 'first_name', 'last_name', 'cccd', 'address']    
+    column_searchable_list = ['first_name', 'last_name', 'cccd', 'address']
+    column_editable_list = ['first_name', 'last_name', 'cccd', 'address']
+
+    can_delete = False
+    can_edit = True
+    can_create = True
+
+class BookedRoomView(AuthenticatedAdmin):
+    column_list = ['id', 'booking_id', 'room_id', 'price', 'created_at']    
+    column_searchable_list = ['booking_id', 'room_id', 'price', 'created_at']
+    column_editable_list = ['booking_id', 'room_id', 'price']
+
+    can_delete = True
+    can_edit = True
+    can_create = True
+    
 
 admin = Admin(app=app, name='QUẢN TRỊ KHÁCH SẠN', template_mode='bootstrap4')
-admin.add_view(ModelView(Room, db.session, name="Phòng"))
-admin.add_view(ModelView(RoomType, db.session, name="Loại Phòng"))
-admin.add_view(ModelView(Guest, db.session, name="Khách hàng"))
-admin.add_view(ModelView(Payment, db.session, name="Thanh toán"))
-admin.add_view(ModelView(PaymentMethod, db.session, name="Phương thức thanh toán"))
-admin.add_view(ModelView(AdditionalPrice, db.session, name="Giá bổ sung"))
-admin.add_view(ModelView(BookedRoom, db.session, name="BookedRoom"))
-admin.add_view(ModelView(Booking, db.session, name="Booking"))
-admin.add_view(ModelView(BookingStatus, db.session, name="Phương thức book"))
-admin.add_view(StatsView(name='Thông kê báo cáo'))
+admin.add_view(ModelView(RoomView(Room, db.session)))
+admin.add_view(ModelView(RoomTypeView(RoomType, db.session)))
+admin.add_view(ModelView(GuestView(Guest, db.session)))
+admin.add_view(ModelView(PaymentView(Payment, db.session)))
+admin.add_view(ModelView(PaymentMethodView(PaymentMethod, db.session)))
+admin.add_view(ModelView(AdditionalPriceView(AdditionalPrice, db.session)))
+admin.add_view(ModelView(BookedRoomView(BookedRoom, db.session)))
+admin.add_view(ModelView(BookingView(Booking, db.session)))
+admin.add_view(ModelView(StatusView(BookingStatus, db.session)))
+admin.add_view(StatsView(name='Thống kê báo cáo'))
 admin.add_view(LogoutView(name='Đăng xuất'))
